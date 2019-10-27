b739f30f2de9133fa2bc6120dd12b2e57e035b8d
jnhallquist <jnhallquist@gmail.com>
2019-10-27 00:44:00 -0700

    Create set reminder cmd

--- bot.py
+++ bot.py
@@ -18,4 +18,21 @@ async def on_message(message):
     if message.content.startswith('$rbot help'):
         await message.author.send('TBA: HELP INFORMATION')
 
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$rbot set reminder'):
+        def check(m):
+          return m.content
+
+        await message.author.send('What is the reminder you would like to set?')
+        msg = await client.wait_for('message', check=check)
+        reminder_content = msg.content
+
+        await message.author.send('When would you like to be reminded?')
+        msg = await client.wait_for('message', check=check)
+        reminder_datetime = msg.content
+
+        print((reminder_content, reminder_datetime))
+
 client.run(secrets["bot_token"])
