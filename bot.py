db38bfd21316a9f550e9ccbd462d1477d5ab190e
jnhallquist <jnhallquist@gmail.com>
2019-10-26 23:58:17 -0700

    Create help cmd

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$rbot help'):
+        await message.author.send('TBA: HELP INFORMATION')
 
 client.run(secrets["bot_token"])
